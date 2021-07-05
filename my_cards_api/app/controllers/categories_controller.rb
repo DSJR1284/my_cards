dba6e1f7987819e7694a03ef8dc4d46567a6ee7c
Doug Jones <DSJR1284@GMAIL.COM>
2021-07-05 16:36:56 -0400

    set up show action in category controller

--- my_cards_api/app/controllers/categories_controller.rb
+++ my_cards_api/app/controllers/categories_controller.rb
@@ -42,7 +42,7 @@ class CategoriesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_category
-      @category = Category.find(params[:id])
+      @category = Category.find_by(sport: params[:id].downcase)
     end
 
     # Only allow a list of trusted parameters through.
