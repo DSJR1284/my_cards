e7ecd6a891c06dc76e4bd157dd1b2e49e55fad29
Doug Jones <DSJR1284@GMAIL.COM>
2021-07-05 16:27:50 -0400

    set up seed data

--- my_cards_api/db/seeds.rb
+++ my_cards_api/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Category.create(sport: "football")
+
+# Category.create(sport: "basketball")
+
+# Category.create(sport: "baseball")
+
+# Category.create(sport: "hockey")
+
+
+ Card.create(name: "Micheal Vick", image: "https://www.biography.com/.image/t_share/MTIwNjA4NjMzNjA2MzQyMTU2/michael-vick-241100-1-402.jpg", category_id: 1, team: "Atlanta Falcons")
+
+ Card.create(name: "Derrick Rose", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4WUS4sy2rM7nHdEG18d1qZ-gWAoaA2PXipQ&usqp=CAU", category_id: 2, team: "Chicago Bulls")
