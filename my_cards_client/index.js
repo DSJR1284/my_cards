49ac79091289d8b4b632e50939af2da4e714ac5e
Doug Jones <DSJR1284@GMAIL.COM>
2021-07-08 13:18:44 -0400

    test card form

--- my_cards_client/index.js
+++ my_cards_client/index.js
@@ -3,6 +3,13 @@ const categoryCall = new CategoryCall(baseUrl)
 const cardCall = new CardCall(baseUrl)
 const cardForm = document.querySelector("#form-container")
 const showCard = document.querySelector("#card-container")
+// const addCard = document.querySelector("#card_form-container > input[type=submit]:nth-child(9)")
+
+Card.cardForm.addEventListener('submit', addACard)
 
 cardCall.getCards()
 Card.displayCardForm()
+
+function addACard(){
+    debugger;
+}
--- my_cards_client/src/card.js
+++ my_cards_client/src/card.js
@@ -3,15 +3,16 @@ class Card{
     
     static all = []
     static showCard = document.querySelector("#card-container")
+    static cardForm = document.querySelector("#form-container")
 
-    constructor(id, name, image, team, category_id){
+    constructor({id, name, image, team, category_id}){
         this.id = id 
         this.name = name 
         this.image = image
         this.team = team 
         this.category_id = category_id
 
-        this.element = document.createElement('li')
+        this.element = document.createElement('div')
         this.element.dataset.id = this.id
         this.element.id = `card-${this.id}`
         // this.element.addEventListener('click', this.handleCreateCard)
@@ -23,8 +24,8 @@ class Card{
     cardHTML(){       
         this.element.innerHTML += `            
             <div>
+            <h3>${this.name}</h3>
                 <img src='${this.image}' width = 150px />
-                <h3>${this.name}</h3>
                 <h3>${this.team}</h3>
             </div>`
 
@@ -32,29 +33,29 @@ class Card{
     }
 
     displayCards(){
-        debugger;
-        Card.showCard.appendChild(this.cardHTML())
+        
+        Card.showCard.append(this.cardHTML())
     }
 
     static displayCardForm(){
-        cardForm.innerHTML = `<div id="card_form-container">
-        <label for="name">Name:</label>
-        <input type="text" id="name">
-        <label for="image">Image:</label>
-        <input type="text" id="image">
-        <label for="team">Team:</label>
-        <input type="text" id="team">
-        <label for="sport">Sport:</label>
-        <select name="sport" id="sport">
-            <option selected disabled hidden>Select Sport</option>
-            <option value="basketball">Basketball</option>
-            <option value="baseball">Baseball</option>
-            <option value="football">Football</option>
-            <option value="hockey">Hockey</option>
-        </select>
-        <input type="submit" value="Add A Card"
-        >
-    </div>`
+        cardForm.innerHTML = `
+        <form id="card_form-container">
+            <label for="name">Name:</label>
+            <input type="text" id="name">
+            <label for="image">Image:</label>
+            <input type="text" id="image">
+            <label for="team">Team:</label>
+            <input type="text" id="team">
+            <label for="sport">Sport:</label>
+            <select name="sport" id="sport">
+                <option selected disabled hidden>Select Sport</option>
+                <option value="basketball">Basketball</option>
+                <option value="baseball">Baseball</option>
+                <option value="football">Football</option>
+                <option value="hockey">Hockey</option>
+            </select>
+                <input type="submit" id="create" value="Add A Card">
+        </form>`
     }
 
 
