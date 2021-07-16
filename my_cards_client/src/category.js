808d8dfe1fe3796ce773bd52f5cdc172cff7d8c5
Doug Jones <DSJR1284@GMAIL.COM>
2021-07-16 14:24:38 -0400

    set up link tags

--- my_cards_client/src/category.js
+++ my_cards_client/src/category.js
@@ -5,13 +5,23 @@ class Category{
     
     constructor({id, sport}){
         this.id = id 
-        this.sport = sport     
-
+        this.sport = sport 
+        
+        this.sport = document.createElement('div')
         Category.all(this)
     }
 
     sportHTML(){
-        this.sport
+        this.sport.innerHTML += `
+            <link class="football"> 
+            </link>
+            <link class="basketball"> 
+            </link>
+            <link class="baseball"> 
+            </link>
+            <link class="hockey"> 
+            </link>
+            `
     }
 
     static displaySportsLink(){
